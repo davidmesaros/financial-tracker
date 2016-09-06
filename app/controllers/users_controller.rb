4c39cc43a1419b86201613de0f6982ebc96b4971
davidmesaros <david.mesaros@hotmail.com>
2016-09-06 23:43:20 +1000

    add migration also devise_name_laste

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
     @user_stocks = current_user.stocks 
     @user = current_user
   end
+
+  def full_name
+      return "#{first_name} #{last_name}".strip if (first_name || last_name)
+        "Anonymous"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   
 
-  devise_for :users
+  
+  devise_for :users, controllers: {registrations: 'user/registrations' }
   root 'welcome#index'
   resources :user_stocks, except: [:show, :edit, :update]
   # get 'users/my_portfolio'
